260cc5c48e09f970c999bda7901d64504e22201f
Liam Aharon <liam.aharon@hotmail.com>
2020-02-26 13:34:52 +1100

    Log process logs and errors

--- index.js
+++ index.js
@@ -116,6 +116,9 @@ pm2.launchBus(function(err, bus) {
     if (moduleConfig.log) {
         bus.on('log:out', function(data) {
             if (data.process.name === 'pm2-slack') { return; } // Ignore messages of own module.
+            
+            console.log('log:out')
+            console.log(data);
 
             const parsedLog = parseIncommingLog(data.data);
             slackUrlRouter.addMessage({
@@ -131,6 +134,9 @@ pm2.launchBus(function(err, bus) {
     if (moduleConfig.error) {
         bus.on('log:err', function(data) {
             if (data.process.name === 'pm2-slack') { return; } // Ignore messages of own module.
+
+            console.log('log:err')
+            console.log(data);
 
             const parsedLog = parseIncommingLog(data.data);
             slackUrlRouter.addMessage({
